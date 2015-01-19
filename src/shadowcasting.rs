5f199541cc244ae641248aa3a08b54d498ad0b9e
root <root@localhost>
2015-01-20 00:27:31 +0100

    Extract common base circle iterator

--- src/shadowcasting.rs
+++ src/shadowcasting.rs
@@ -32,16 +32,7 @@ fn visible_when(center: bool, near: bool, far: bool) -> bool
 /// with visibility **true**.
 pub struct RPAShadowcasting<F> {
     obstruct: F,
-    obstructions: Vec<(f32, f32)>,
-    /// identify x-y quadrant and top/bottom half of quadrant.
-    /// cycle (1, 1) -> (1, -1) -> (-1, -1) -> (-1, 1)
-    octant: (i32, i32, bool),
-    /// size of the circle
-    radius: i32,
-    /// radial coordinate
-    r: i32,
-    /// transversal coordinate
-    x: i32,
+    iter: CircleIter<()>,
 }
 
 impl<F> RPAShadowcasting<F> where
@@ -54,29 +45,7 @@ impl<F> RPAShadowcasting<F> where
     pub fn new(radius: i32, obstruct: F) -> Self {
         RPAShadowcasting {
             obstruct: obstruct,
-            obstructions: Vec::new(),
-            octant: (1, 1, true),
-            radius: radius,
-            r: 0,
-            x: 0,
-        }
-    }
-
-    fn next_octant(&mut self)
-    {
-        let (ref mut x, ref mut y, ref mut vert) = self.octant;
-        *vert = !*vert;
-        if !*vert {
-            return;
-        }
-        if *x > 0 && *y > 0 {
-            *y = -1;
-        } else if *x > 0 && *y < 0 {
-            *x = -1;
-        } else if *x < 0 && *y < 0 {
-            *y = 1;
-        } else {
-            *x = 1;
+            iter: CircleIter::new(radius),
         }
     }
 }
@@ -108,56 +77,20 @@ impl<F> Iterator for RPAShadowcasting<F> where
     /// and far edge and compare with all previous recorded occlusions.
     fn next(&mut self) -> Option<(i32, i32, bool)>
     {
-        if self.r == 0 {
-            self.r += 1;
-            return Some((0, 0, true));
-        }
-        if self.x > self.r {
-            self.x = 0;
-            self.r += 1;
-        }
-
-        // Skip to next octant when we reach the radial limit.
-        if self.r > self.radius {
-            self.next_octant();
-            self.x = 0;
-            self.r = 1;
-            self.obstructions.clear();
-            if self.octant == (1,1,true) {
-                // back at the original octant and done.
-                return None;
-            } else {
-                return self.next();
-            }
-        }
-
-        let (qx, qy, vert) = self.octant;
-
-        let (a, b) = if vert {
-            (self.x * qx, self.r * qy)
-        } else {
-            (self.r * qx, self.x * qy)
+        let (a, b, (near, center, far)) = match self.iter.next() {
+            None => return None,
+            Some(x) => x,
         };
-
-        if Float::hypot(a as f32, b as f32) >= RADIUS_FUDGE + self.radius as f32 {
-            self.x += 1;
-            return self.next();
-        }
-
-        let angle_alloc = 1. / ((self.r + 1) as f32);
-        let near_angle = (self.x as f32) * angle_alloc;
-        let center_angle = near_angle + 0.5 * angle_alloc;
-        let far_angle = near_angle + angle_alloc;
 
         // check visibility vs prev obstructions.
         let mut visible = true;
         let mut near_vis = true;
         let mut center_vis = true;
         let mut far_vis = true;
-        for &(near_obs, far_obs) in self.obstructions.iter() {
-            near_vis = near_vis && !angle_contained_in(near_angle, near_obs, far_obs);
-            center_vis = center_vis && !angle_contained_in(center_angle, near_obs, far_obs);
-            far_vis = far_vis && !angle_contained_in(far_angle, near_obs, far_obs);
+        for &(near_obs, far_obs, _) in self.iter.obstructions.iter() {
+            near_vis = near_vis && !angle_contained_in(near, near_obs, far_obs);
+            center_vis = center_vis && !angle_contained_in(center, near_obs, far_obs);
+            far_vis = far_vis && !angle_contained_in(far, near_obs, far_obs);
 
             visible = visible_when(center_vis, near_vis, far_vis);
             if !visible {
@@ -165,9 +98,8 @@ impl<F> Iterator for RPAShadowcasting<F> where
             }
         }
         if (NONVIS_OCCLUDE && !visible) || (self.obstruct)(a, b) {
-            self.obstructions.push((near_angle, far_angle));
+            self.iter.obstructions.push((near, far, ()));
         }
-        self.x += 1;
         Some((a, b, visible))
     }
 }
@@ -185,16 +117,7 @@ impl<F> Iterator for RPAShadowcasting<F> where
 pub struct RPAPartialShadowcasting<F> {
     obstruct: F,
     /// begin angle, end angle, opacity in 0. to 1.
-    obstructions: Vec<(f32, f32, f32)>,
-    /// identify x-y quadrant and top/bottom half of quadrant.
-    /// cycle (1, 1) -> (1, -1) -> (-1, -1) -> (-1, 1)
-    octant: (i32, i32, bool),
-    /// size of the circle
-    radius: i32,
-    /// radial coordinate
-    r: i32,
-    /// transversal coordinate
-    x: i32,
+    iter: CircleIter<f32>,
 }
 
 impl<F> RPAPartialShadowcasting<F> where
@@ -207,6 +130,83 @@ impl<F> RPAPartialShadowcasting<F> where
     pub fn new(radius: i32, obstruct: F) -> Self {
         RPAPartialShadowcasting {
             obstruct: obstruct,
+            iter: CircleIter::new(radius),
+        }
+    }
+}
+
+impl<F> Iterator for RPAPartialShadowcasting<F> where
+    F: FnMut(i32, i32) -> f32,
+{
+    /// The iterator element represents **(x, y, opacity)** in coordinates
+    /// relative to the center, opacity from 0. to 1. (1. means no visibility).
+    type Item = (i32, i32, f32);
+
+    fn next(&mut self) -> Option<(i32, i32, f32)>
+    {
+        let (a, b, (near, center, far)) = match self.iter.next() {
+            None => return None,
+            Some(x) => x,
+        };
+
+        // check visibility vs prev obstructions.
+        // preserve the same visible_when rule as in the discrete case
+        let mut opacity = 0.;
+        let mut near_vis = true;
+        let mut center_vis = true;
+        let mut far_vis = true;
+        for &(near_obs, far_obs, opc_obs) in self.iter.obstructions.iter() {
+            near_vis = near_vis && !angle_contained_in(near, near_obs, far_obs);
+            center_vis = center_vis && !angle_contained_in(center, near_obs, far_obs);
+            far_vis = far_vis && !angle_contained_in(far, near_obs, far_obs);
+            if !center_vis {
+                opacity = opacity.max(0.5 * opc_obs);
+            }
+            if !visible_when(center_vis, near_vis, far_vis) {
+                opacity = opacity.max(opc_obs);
+            }
+
+            if opacity >= 1. {
+                break;
+            }
+        }
+        let mut opc_here = (self.obstruct)(a, b);
+        opc_here = opacity + opc_here;
+        if opc_here > 0. {
+            self.iter.obstructions.push((near, far, opc_here));
+        }
+        opacity = opacity.min(1.);
+        Some((a, b, opacity))
+    }
+}
+
+/// Visit all squares in a circle around (0, 0) up until the specified radius.
+/// The iterator computes the near, center, far angles for each square.
+///
+/// Note: May visit a few coordinates twice.
+#[derive(Clone, Show)]
+struct CircleIter<T> {
+    /// This is cleared every time we switch octant.
+    pub obstructions: Vec<(f32, f32, T)>,
+    /// identify x-y quadrant and top/bottom half of quadrant.
+    /// cycle (1, 1) -> (1, -1) -> (-1, -1) -> (-1, 1)
+    octant: (i32, i32, bool),
+    /// size of the circle
+    radius: i32,
+    /// radial coordinate
+    r: i32,
+    /// transversal coordinate
+    x: i32,
+}
+
+/// x, y, (near, center, far).
+type CircleItem = (i32, i32, (f32, f32, f32));
+
+impl<T> CircleIter<T>
+{
+    /// Create a new **CircleIter**.
+    pub fn new(radius: i32) -> Self {
+        CircleIter {
             obstructions: Vec::new(),
             octant: (1, 1, true),
             radius: radius,
@@ -234,18 +234,15 @@ impl<F> RPAPartialShadowcasting<F> where
     }
 }
 
-impl<F> Iterator for RPAPartialShadowcasting<F> where
-    F: FnMut(i32, i32) -> f32,
+impl<T> Iterator for CircleIter<T>
 {
-    /// The iterator element represents **(x, y, opacity)** in coordinates
-    /// relative to the center, opacity from 0. to 1. (1. means no visibility).
-    type Item = (i32, i32, f32);
+    type Item = CircleItem;
 
-    fn next(&mut self) -> Option<(i32, i32, f32)>
+    fn next(&mut self) -> Option<CircleItem>
     {
         if self.r == 0 {
             self.r += 1;
-            return Some((0, 0, 0.));
+            return Some((0, 0, (0., 0., 0.)));
         }
         if self.x > self.r {
             self.x = 0;
@@ -278,40 +275,14 @@ impl<F> Iterator for RPAPartialShadowcasting<F> where
             self.x += 1;
             return self.next();
         }
-
+        // compute square's angles.
         let angle_alloc = 1. / ((self.r + 1) as f32);
-        let near_angle = (self.x as f32) * angle_alloc;
-        let center_angle = near_angle + 0.5 * angle_alloc;
-        let far_angle = near_angle + angle_alloc;
+        let near = (self.x as f32) * angle_alloc;
+        let center = near + 0.5 * angle_alloc;
+        let far = near + angle_alloc;
 
-        // check visibility vs prev obstructions.
-        // preserve the same visible_when rule as in the discrete case
-        let mut opacity = 0.;
-        let mut near_vis = true;
-        let mut center_vis = true;
-        let mut far_vis = true;
-        for &(near_obs, far_obs, opc_obs) in self.obstructions.iter() {
-            near_vis = near_vis && !angle_contained_in(near_angle, near_obs, far_obs);
-            center_vis = center_vis && !angle_contained_in(center_angle, near_obs, far_obs);
-            far_vis = far_vis && !angle_contained_in(far_angle, near_obs, far_obs);
-            if !center_vis {
-                opacity = opacity.max(0.5 * opc_obs);
-            }
-            if !visible_when(center_vis, near_vis, far_vis) {
-                opacity = opacity.max(opc_obs);
-            }
-
-            if opacity >= 1. {
-                break;
-            }
-        }
-        let mut opc_here = (self.obstruct)(a, b);
-        opc_here = opacity + opc_here;
-        if opc_here > 0. {
-            self.obstructions.push((near_angle, far_angle, opc_here));
-        }
         self.x += 1;
-        opacity = opacity.min(1.);
-        Some((a, b, opacity))
+        Some((a, b, (near, center, far)))
     }
 }
+
